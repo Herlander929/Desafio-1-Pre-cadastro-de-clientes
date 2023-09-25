4530cc7b6f98b5a98714450c8d838f6dab36c563
Herlander <herlander97dasilva@gmail.com>
2023-09-25 20:10:46 -0300

    Creating project

---
+++ src/test/java/Desafio1/Precadastro/de/clientes/PreCadastroDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package Desafio1.Precadastro.de.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreCadastroDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
