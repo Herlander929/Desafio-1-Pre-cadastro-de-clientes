7fe95a655644cb54620b3c354da7f5092dfe7a0c
Herlander <herlander97dasilva@gmail.com>
2023-09-25 21:40:25 -0300

    Creating repository for client

--- src/main/java/Desafio1/Precadastro/de/clientes/PreCadastroDeClientesApplication.java
+++ src/main/java/Desafio1/Precadastro/de/clientes/PreCadastroDeClientesApplication.java
@@ -1,10 +1,16 @@
 package Desafio1.Precadastro.de.clientes;
 
+import Desafio1.Precadastro.de.clientes.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PreCadastroDeClientesApplication {
+
+	@Autowired
+	private ClientRepository clientRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PreCadastroDeClientesApplication.class, args);
--- src/main/java/Desafio1/Precadastro/de/clientes/repositories/ClientRepository.java
+++ src/main/java/Desafio1/Precadastro/de/clientes/repositories/ClientRepository.java
@@ -1,2 +1,9 @@
-package Desafio1.Precadastro.de.clientes.repositories;public class ClientRepository {
+package Desafio1.Precadastro.de.clientes.repositories;
+
+import Desafio1.Precadastro.de.clientes.domain.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Integer> {
 }
